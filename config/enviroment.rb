6832aeb5e1e35dd98639b1600d6690c041656f8f
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-02-04 17:07:16 -0500

    requires all files and gems

---
+++ config/enviroment.rb
@@ -0,0 +1,6 @@
+require "pry"  #require takes an absolute path
+require "httparty"
+
+require_relative("../lib/api")  #require relative takes a path in relation  to the file
+require_relative("../lib/cli")
+require_relative("../lib/location")
