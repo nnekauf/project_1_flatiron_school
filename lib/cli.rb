de35cbf8b5d91c051a5ac43472a81c0380d10f87
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-02-04 22:11:30 -0500

    slightly changes puts statements in introduction method and finishes main_menu method within the Cli class

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,22 +2,30 @@ class Cli
 
     def introduction
    # 1. USER WILL BE WELCOMED: who we are, what we do, let them know we have info from 1st semester of 2020      
-        puts "Welcome to Shooting Crimes CLI Application."
-        puts "We provide information on shootings documented by the NYPD that occured within the first semester of 2020 in New York City."
-        puts "Though this information is subject to your own discretion and interpretation, we hope this application reveals patterns within the data that helps citizens of NYC become more vigilant while also inspiring safer communities."
+        puts "INTRODUCTION"
+        puts "  Welcome to Shooting Crimes CLI Application."
+        puts "  We provide information on shootings documented by the NYPD that occured in New York City within the first semester of 2020."
+        puts "  Though this information is subject to your own discretion and interpretation, we hope this application reveals patterns within the data that helps citizens of NYC become more vigilant while also inspiring safer communities."
+        self.main_menu
     end
 
     def main_menu
 #     2. USER WILL BE TOLD WHAT THEY HAVE AVAILABLE TO SEE:
-        
+#     3. USER WILL BE ASKED TO GIVE BORO IN ORDER TO GET MORE SPECIFIED DATA: 
+#         to get started please select a borough from these options (type options) by typing its name below. 
+#(will have to account for case sensitivity, make sure respone and option equak eachother in downcase
+        puts "MAIN MENU"
+        puts "  To access information on Shootings that occured within NYC during the first semester of 2020, please select a Borough from the options below."
+        puts "  OPTIONS:[ BRONX | BROOKLYN | MANHATTAN | QUEENS | STATEN ISLAND ]"
+        puts "  Please type the name of any borough to continue."
+        input = gets.strip
     end
 
 
 
     
     
-    #     3. USER WILL BE ASKED TO GIVE BORO IN ORDER TO GET MORE SPECIFIED DATA: 
-    #         to get started please select a borough from these options (type options) by typing its name below. (will have to account for case sensitivity)
+    
     #     4. WE GET USER INPUT AND CHECK FOR VALIDITY. 
     #     5. IF INPUT IS VALID, WILL ASK USER TO CHOOSE MONTH BTWN JAN - JULY
     #     6. WE GET USER INPUT AND CHECK FOR VALIDITY.
