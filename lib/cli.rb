2c5f2c2d98343cdb44462b754751e86bca691cec
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-02-06 22:21:01 -0500

    reorganizes Cli methods and adds get_word method

--- lib/cli.rb
+++ lib/cli.rb
@@ -13,19 +13,20 @@ class Cli
         puts "  You're thinking of a word... any word...."
         puts "  Make sure it's ONE word and spelled properly or else you'll bump into an error!"
         puts "  What's the Word? Enter it below."
-        @word = gets.strip
-        new_word = Api.get_thesaurus_by_word(@word)
-        #binding.pry
-        #  #may need to seperate in its own method
-        # self.return_word_info #will need to move this for seperation of duties
     end
 
-    def valid_word?
-         #     4. CHECK FOR VALIDITY. 
-         #@word =~ /(\d|\s)/  #the word is valid if it does not include spaces or a number
-                                  #the word is valid if the regex does not return nil
-                                    #the word is valid if the thesarus does not return an empty array
+    def get_word
+        word = gets.strip
+        new_word = Api.get_thesaurus_by_word(word)
+        #binding.pry
     end
+
+    # def valid_word?
+    #      #     4. CHECK FOR VALIDITY. 
+    #      #@word =~ /(\d|\s)/  #the word is valid if it does not include spaces or a number
+    #                               #the word is valid if the regex does not return nil
+    #                                 #the word is valid if the thesarus does not return an empty array
+    # end
 
     def return_word_info
         #if valid_word?
