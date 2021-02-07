bc8a1a7e0a78617abc9ad52d3bb6e2766721db61
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-02-06 21:57:32 -0500

    updates the notes file with an outline for the project

--- lib/api.rb
+++ lib/api.rb
@@ -5,7 +5,7 @@ class Api
 # Key (Dictionary): 85a434fc-3a5d-48df-a8e0-35b0c5bd54ee
 
 
-#url_interpolated = https://www.dictionaryapi.com/api/v3/references/thesaurus/json/#{word}?key=299e3474-8d57-473c-836c-9e5c31d32f6e
+
     
     def self.get_thesaurus_by_word(word)
         url = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/#{word}?key=299e3474-8d57-473c-836c-9e5c31d32f6e"
--- lib/cli.rb
+++ lib/cli.rb
@@ -12,10 +12,7 @@ class Cli
     end
 
     def main_menu
-#     2. USER WILL BE TOLD WHAT THEY HAVE AVAILABLE TO SEE:
-#     3. USER WILL BE ASKED TO GIVE BORO IN ORDER TO GET MORE SPECIFIED DATA: 
-#         to get started please select a borough from these options (type options) by typing its name below. 
-#(will have to account for case sensitivity, make sure respone and option equak eachother in downcase
+
         puts "MAIN MENU"
         puts "  You're thinking of a word... any word...."
         puts "  Make sure it's ONE word and spelled properly or else you'll bump into an error!"
@@ -47,12 +44,5 @@ class Cli
     
     
    
-    #     5. IF INPUT IS VALID, WILL ASK USER TO CHOOSE MONTH BTWN JAN - JULY
-    #     6. WE GET USER INPUT AND CHECK FOR VALIDITY.
-    #     7. RETURN DIFFERENT SHOOTING INCIDENCES WITHIN BORO AND MONTH CHOSEN
-    #         will show  DATE AND TIME, SUSPECT INFO: AGE, SEX, RACE(MAYBE), VICTIM INFO: AGE, SEX, RACE(MAYBE), IF THE VICTIM DIED
-    #     8. THEY SHOULD HAVE THE OPTION TO GO TO MAIN MENU
-    #     8. ALL USER INPUTS SHOULD BE VALID
-    #     9. THEY SHOULD HAVE THE OPTION TO EXIT THE APP WHEN THEY WANT
-    #     10. MAKE SURE IM STORING THE DATA SO I DONT HAVE TO DO IDENTICAL API REQUESTS
+
 end
