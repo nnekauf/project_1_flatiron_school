3b738ddf829fbe8ffcbae7678119428f560fbff0
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-02-07 03:37:04 -0500

    adds different methods to check for validity of word and if the attribute of that word exists

--- lib/api.rb
+++ lib/api.rb
@@ -1,63 +1,82 @@
 require "pry"
 class Api
     #  Key (Thesaurus): 299e3474-8d57-473c-836c-9e5c31d32f6e
+    # Key (Dictionary): 85a434fc-3a5d-48df-a8e0-35b0c5bd54ee
 
-# Key (Dictionary): 85a434fc-3a5d-48df-a8e0-35b0c5bd54ee
-
-
-
-    
     def self.get_thesaurus_by_word(word)
         url = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/#{word}?key=299e3474-8d57-473c-836c-9e5c31d32f6e"
         response = HTTParty.get(url)
-        main_hash = response[0] 
-# def valid_word?
-    #the word isnt valid if the thesarus returns an empty array
-    #the word isnt valid if the name method that the thesaurs reetuns does not equal the word typed
-    #the word may not have an antonym, synonym, related word, or short definition. for example "talk" has no antonym
-    # end
-        # if main_hash["meta"]["id"] != "#{word}"
-        #     return false
-        # elsif response.empty?
-        #     return false
-    
-        # end
-#binding.pry
-        if !main_hash["def"][0]["sseq"][0][0][1].flatten.include?("rel_list") 
-            name_hash = {related_words: "We're sorry! We couldn't find any related words."}
-        elsif !main_hash["meta"].flatten.include?("syns")  #if the api response does not include the words rel_list, syns, ants, or shortdef...
-            name_hash = {syns: "We're sorry! We couldn't find any synonyms."}                                      #...I know that the word requested does not have that specific attriubute that wasn't included
-        elsif !main_hash["meta"].flatten.include?("ants")   #since that word doesnt have either a syn,ant,rel_word,or short_def, I would have to set that attribute to "none"...
-            name_hash = {ants: "We're sorry! We couldn't find any antonyms."}                                       #... but still be able to return the attributes that do exist.
-                                                                                                        
-        elsif !main_hash.flatten.include?("shortdef")
-            name_hash = {short_def: "We're sorry! We couldn't find any definitions."}
-        else
-            rel_words = []
-                main_hash["def"][0]["sseq"][0][0][1]["rel_list"].each do |array| 
-                    array.each{|hash| hash.each {|key,value| rel_words << "#{value}"}}
-                end
-            
-            syn_words = []
-                main_hash["meta"]["syns"].each do |array|
-                    array.each{|word| syn_words << word}
-                end
-
-            ant_words = []
-                main_hash["meta"]["ants"].each do |word| #
-                ant_words << word
-                end
-
-            name_hash = {name: main_hash["meta"]["id"], syns: syn_words.join(", "),
-            ants: ant_words.join(", "),
-            related_words: rel_words.join(", "), 
-            short_def: main_hash["shortdef"][0] 
-            } 
-            
-        end
-        Word.new(name_hash)
-         
+        @main_hash = response[0] 
     end
+
+    def valid_word?
+        if @main_hash["meta"]["id"] != "#{word}"
+            return false
+        elsif response.empty?
+            return false
+         end
+    end
+
+    def valid_rel_list?
+        @main_hash["def"][0]["sseq"][0][0][1].flatten.include?("rel_list")
+    end
+
+    def valid_syns?
+        @main_hash["meta"].flatten.include?("syns")
+    end
+
+    def valid_ants?
+        @main_hash["meta"].flatten.include?("ants")
+    end
+
+    def valid_shortdef?
+        @main_hash.flatten.include?("shortdef")
+    end
+
+    def assign_attributes
+        name_hash = {name: @main_hash["meta"]["id"]}
+
+        if valid_rel_list?
+            rel_words = []
+            @main_hash["def"][0]["sseq"][0][0][1]["rel_list"].each do |array| 
+                array.each{|hash| hash.each {|key,value| rel_words << "#{value}"}}
+            end
+            name_hash = {related_words: rel_words.join(", ")}
+        else 
+            name_hash = {related_words: "We're sorry! We couldn't find any related words."}
+        end
+
+        if valid_syns?
+            syn_words = []
+            @main_hash["meta"]["syns"].each do |array|
+                array.each{|word| syn_words << word}
+            end
+            name_hash = { syns: syn_words.join(", ")}
+        else
+            name_hash = {syns: "We're sorry! We couldn't find any synonyms."}
+        end
+
+        if valid_ants?
+            ant_words = []
+            @main_hash["meta"]["ants"].each do |word| #
+            ant_words << word
+            end
+            name_hash = {ants: ant_words.join(", ")}
+        else
+            name_hash = {ants: "We're sorry! We couldn't find any antonyms."}
+        end
+
+        if valid_shortdef?
+            name_hash = {short_def: @main_hash["shortdef"][0]}
+        else
+            name_hash = {short_def: "We're sorry! We couldn't find any definitions."}
+        end
+        binding.pry
+    end
+    
+    #Word.new(name_hash)
+         
+    
    
    
 
@@ -71,3 +90,35 @@ end
         # end
 
         #new word has to equal the name property in the hash in either upper or lowercase form, for examply hi.
+
+
+        # if !main_hash["def"][0]["sseq"][0][0][1].flatten.include?("rel_list") 
+        #     name_hash = {related_words: "We're sorry! We couldn't find any related words."}
+        # elsif !main_hash["meta"].flatten.include?("syns")  #if the api response does not include the words rel_list, syns, ants, or shortdef...
+        #     name_hash = {syns: "We're sorry! We couldn't find any synonyms."}                                      #...I know that the word requested does not have that specific attriubute that wasn't included
+        # elsif !main_hash["meta"].flatten.include?("ants")   #since that word doesnt have either a syn,ant,rel_word,or short_def, I would have to set that attribute to "none"...
+        #     name_hash = {ants: "We're sorry! We couldn't find any antonyms."}                                       #... but still be able to return the attributes that do exist.
+                                                                                                        
+        # elsif !main_hash.flatten.include?("shortdef")
+        #     name_hash = {short_def: "We're sorry! We couldn't find any definitions."}
+        # else
+        #     rel_words = []
+        #     main_hash["def"][0]["sseq"][0][0][1]["rel_list"].each do |array| 
+        #         array.each{|hash| hash.each {|key,value| rel_words << "#{value}"}}
+        #     end
+        
+        # syn_words = []
+        #     main_hash["meta"]["syns"].each do |array|
+        #         array.each{|word| syn_words << word}
+        #     end
+
+        # ant_words = []
+        #     main_hash["meta"]["ants"].each do |word| #
+        #     ant_words << word
+        #     end
+
+        # name_hash = {name: main_hash["meta"]["id"], syns: syn_words.join(", "),
+        # ants: ant_words.join(", "),
+        # related_words: rel_words.join(", "), 
+        # short_def: main_hash["shortdef"][0] 
+        # } 
